7817f9988a1e043ce6b135efef2e8ebabc56d6d9
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-07-11 15:45:14 -0300

    feat: add api calls to get info about the trip, activities and guests into the trip details page

--- src/pages/trip-details/create-activity-modal.tsx
+++ src/pages/trip-details/create-activity-modal.tsx
@@ -1,11 +1,31 @@
 import { Calendar, Tag, X } from 'lucide-react';
+import { FormEvent } from 'react';
+import { useParams } from 'react-router-dom';
 import { Button } from '../../components/button';
+import { api } from '../../lib/axios';
 
 interface CreateActivityModalProps {
 	closeCreateActivityModal: () => void;
 }
 
 export function CreateActivityModal({ closeCreateActivityModal }: CreateActivityModalProps) {
+	const { tripId } = useParams();
+
+	async function createActivity(event: FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+
+		const data = new FormData(event.currentTarget);
+		const title = data.get('title')?.toString();
+		const occurs_at = data.get('occurs_at')?.toString();
+
+		await api.post(`/trips/${tripId}/activities`, {
+			title,
+			occurs_at
+		});
+
+		window.document.location.reload();
+	}
+
 	return (
 		<div className="fixed inset-0 bg-black/60 flex items-center justify-center">
 			<div className="w-[540px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
@@ -23,7 +43,7 @@ export function CreateActivityModal({ closeCreateActivityModal }: CreateActivity
 					</p>
 				</div>
 
-				<form className="space-y-3">
+				<form onSubmit={createActivity} className="space-y-3">
 					<div className="px-4 h-14 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
 						<Tag className="text-zinc-400 size-5" />
 
@@ -46,7 +66,7 @@ export function CreateActivityModal({ closeCreateActivityModal }: CreateActivity
 						/>
 					</div>
 
-					<Button type="button" size="full">
+					<Button type="submit" size="full">
 						Salvar atividade
 					</Button>
 				</form>
